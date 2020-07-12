08371c308749b19f5c1309c3ea5a74dc70352813
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-07-12 19:09:45 +0900

    delete user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_091731) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
